b2a8f759a317e032cdbd66a5bc3da94e89f5130c
tocvic <vickytochi6882@gmail.com>
2023-06-28 01:35:22 +0100

    new fix

--- 10-loops.js
+++ 10-loops.js
@@ -1,8 +1,17 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const idx of array) {
-	newArray.push(appendString + value);
+    for (var idx in array) {
+      var value = array[idx];
+      array[idx] = appendString + value;
     }
+  
+    return array;
+  }
+  
+//export default function appendToEachArrayValue(array, appendString) {
+    //const newArray = [];
+    //for (const idx of array) {
+	//newArray.push(appendString + value);
+    //}
 
-    return newArray;
-}
+    //return newArray;
+//}
--- 100-createIteratorObject.js
+++ 100-createIteratorObject.js
@@ -1,12 +1,25 @@
 export default function createIteratorObject(report) {
-    const keys = Object.keys(report);
-    return {
-	next() {
-	    const key = keys.shift();
-	    return {
-		value: key,
-		done: key === undefined,
-	    };
-	},
-    };
-}
+	const employees = Object.values(report.dept).flatMap((dept) => dept.employeeList);
+	let index = 0;
+  
+	return {
+	  next() {
+		if (index < employees.length) {
+		  const employeeList = employees[index];
+		  index++;
+		  return {
+			value: employeeList,
+			done: false,
+		  };
+		} else {
+		  return {
+			done: true,
+		  };
+		}
+	  },
+	  [Symbol.iterator]() {
+		return this;
+	  },
+	};
+  }
+  
